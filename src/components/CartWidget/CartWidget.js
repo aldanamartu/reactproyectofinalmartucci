c7b4c91f933e6a716257e3998be3f997280f612f
Aldana Martucci <aldanamartu@gmail.com>
2023-09-02 23:12:42 -0300

    carrito version 2

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,10 +1,24 @@
-import carrito from './assets/carrito.png';
+import img from './assets/carrito.png';
+
+let carrito = localStorage.getItem("carrito");
 
 const CartWidget = () => {
+    if (!carrito || carrito.length == 0) {
+        carrito = [];
+    } else {
+        try {
+            carrito = JSON.parse(carrito);
+        } catch (error) {
+            console.log("carrito ya es json");
+        }
+    }
+    console.log(carrito)
+
+
     return (
         <div>
-            <img src={carrito} style={{ width: '30px', height: '30px' }} alt='imagen carrito'></img>
-            0
+            <img src={img} style={{ width: '30px', height: '30px' }} alt='imagen carrito'></img>
+            { carrito.length }
         </div>
         
     )
--- src/pages/Carrito.js
+++ src/pages/Carrito.js
@@ -3,13 +3,19 @@ import { Link } from "react-router-dom";
 import { db } from "../components/Firebase/firebase"
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { useEffect, useState } from "react";
+import imgEliminar from '../components/CartWidget/assets/eliminar.png';
 
 let carrito = localStorage.getItem("carrito");
 let precio = localStorage.getItem("precio");
 
+const actualizarLocalStorge = (carrito, precio) => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+    localStorage.setItem("precio", JSON.stringify(precio));
+}
+
 function Carrito() {
-    //const itemsCollectionRef = collection(db, "items");
     const [itemList, setItemList] = useState([]);
+    const [counter, updateCounter] = useState([0]);
     let items = collection(db, "items");
 
 
@@ -24,23 +30,41 @@ function Carrito() {
     }
     console.log(carrito)
 
-    var result = [];
-    for (var i in carrito) {
-        result.push(carrito[i].id);
-    }
-
-    const q = query(items, where("title", "in", result));
-
     useEffect(() => {
-        const getItemList = async () => {
-            const data = await getDocs(q);
-            const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-            //console.log(filteredData);
-            setItemList(filteredData);
+        var result = [];
+        for (var i in carrito) {
+            result.push(carrito[i].id);
         }
 
-        getItemList();
-    }, [])
+        if (result.length > 0) {
+            const q = query(items, where("title", "in", result));
+            const getItemList = async () => {
+                const data = await getDocs(q);
+                const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+                setItemList(filteredData);
+            }
+    
+            getItemList();
+        } else {
+            setItemList([]);
+        }
+    
+    }, [counter])
+
+    const handleClickEliminar = event => {
+        console.log(event.target);
+        console.log('eliminar producto %i', event.target.id);
+        console.log(carrito);
+
+        var producto = carrito.find(o => o.id === event.target.id);
+        if (producto.cantidad > 1) {
+            producto.cantidad -= 1;
+        } else {
+            carrito = carrito.filter(i => i.id !== event.target.id);
+        }
+        actualizarLocalStorge(carrito);
+        updateCounter(counter + 1);
+    };
 
     return (
         <div>
@@ -49,10 +73,19 @@ function Carrito() {
             <div className="galeria">
                 {itemList.map((producto) => {
 
-                    var cant = carrito.find(o => o.id === producto.title).cantidad;
+                    var carritoEnProducto = carrito.find(o => o.id === producto.title);
+                    var cant = carritoEnProducto != null ? carritoEnProducto.cantidad : 0;
                     return (
                         <article key={producto.id}>
-                            <h5>{producto.title} ({cant} unidades)</h5>
+                            <h5>
+                                {producto.title} ({cant} unidades)
+                                <img
+                                    src={imgEliminar}
+                                    style={{ width: '20px', height: '20px' }}
+                                    alt='eliminar'
+                                    id={producto.title}
+                                    onClick={handleClickEliminar} />
+                            </h5>
                             <img src={producto.image} alt="productos" />
                             <Link to={`/productos/${producto.id}`}>Mas Info</Link>
                         </article>
